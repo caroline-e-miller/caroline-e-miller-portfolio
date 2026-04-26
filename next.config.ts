c0116349a6805411f4779b95f5c9cab04071d8c8
Caroline Miller <carolinemiller@Carolines-MacBook-Air.local>
2026-04-26 15:39:41 -0500

    troubleshooting deploy

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/caroline-e-miller-portfolio',
   /* config options here */
 };
 
 module.exports = {
+  output: 'export',
   env: {
     customKey: 'my-value',
   },
