683230da9ca7b3019de4ae99ec115b765ae9e3b5
Caroline Miller <carolinemiller@Carolines-MacBook-Air.local>
2026-04-26 16:03:52 -0500

    update paths

--- next.config.ts
+++ next.config.ts
@@ -5,10 +5,15 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   output: 'export',
+  basePath: isProd ? '/caroline-e-miller-portfolio' : '',
+  assetPrefix: isProd ? '/caroline-e-miller-portfolio/' : '',
   env: {
     customKey: 'my-value',
+    trailingSlash: true,
   },
 }
 
