272847e6001f19e9b5c45f3aca703422b9e863e4
Caroline Miller <carolinemiller@Carolines-MacBook-Air.local>
2026-04-26 12:45:21 -0500

    refactor mobile theme toggle, address theme hydrationerror

--- portfolio-2025/src/app/components/primarynav.tsx
+++ portfolio-2025/src/app/components/primarynav.tsx
@@ -99,32 +99,39 @@ export default function PrimaryNav() {
       {isOpen && (
         <>
           <div className="lg:hidden fixed top-24 left-0 right-0 bottom-0 backdrop-blur-sm bg-white/10 z-40" />
-          <div className="lg:hidden fixed top-24 left-0 right-0 bg-white border-b border-gray-200 z-50 w-full">
-            {/* <div className="px-6 py-4 space-y-2"> */}
+          <div className="lg:hidden fixed top-24 left-0 right-0 bg-white dark:bg-black z-50 w-full">
             <div>
               {navigation.map((item) => (
                 <a
                   key={item.name}
                   href={item.href}
-                  className="block px-6 py-4 text-sm font-semibold text-foreground hover:bg-gray-100"
+                  className="block px-6 py-4 text-sm font-semibold text-foreground hover:bg-gray-100 hover:dark:bg-gray-800"
                   onClick={() => setIsOpen(false)}
                 >
                   {item.name}
                 </a>
               ))}
-              <button
-                onClick={toggleTheme}
-                className="block w-full text-left px-6 py-4 text-sm font-semibold text-foreground hover:bg-gray-100"
-                suppressHydrationWarning
-              >
-                {theme === 'dark' ? 'Light Mode' : 'Dark Mode'}
-              </button>
               <a
                 href="https://github.com/caroline-e-miller/portfolio-mcportface"
-                className="block px-6 py-4 text-sm font-semibold text-foreground hover:bg-gray-100"
+                className="block px-6 py-4 text-sm font-semibold text-foreground hover:bg-gray-100 hover:dark:bg-gray-800"
               >
                 GitHub
               </a>
+              <button
+                onClick={toggleTheme}
+                className="block w-full text-left px-6 py-4 text-sm font-semibold text-foreground hover:bg-gray-100 hover:dark:bg-gray-800"
+                suppressHydrationWarning
+              >
+                {theme === 'dark' ? (
+                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+                  </svg>
+                ) : (
+                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+                  </svg>
+                )}
+              </button>
             </div>
           </div>
         </>
